4279adb82f9114968a58dca1303bb7ff8e1f720d
Sergio luiz <segioj995@hotmail.com>
2022-11-18 21:35:19 -0300

    resolução do exercicio

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿int numeroInicial = 0;
+
+Console.ForegroundColor= ConsoleColor.White;
+
+System.Console.Write(" Numeros pares entre 0 - ");
+int numeroLimite = Convert.ToInt32(Console.ReadLine());
+
+Console.ResetColor();
+
+
+Console.ForegroundColor = ConsoleColor.Magenta;
+while (numeroInicial <= numeroLimite)
+
+{
+  System.Console.Write($" {numeroInicial} " );
+  numeroInicial += 2;
+
+}
+
+Console.ResetColor();
+
+System.Console.WriteLine();
